592596374abf00d1a40de5a631f8319ad79d460b
Keven Leone <keven.santos.sz@gmail.com>
2026-01-26 16:10:26 -0300

    Add dryRun to PaginationRun

--- packages/etl/src/core/PaginationRun.ts
+++ packages/etl/src/core/PaginationRun.ts
@@ -1,12 +1,51 @@
 import { APIResponse } from '../types';
 import { logger } from '../utils/logger';
 
+const noop = () => null;
+
 export default class PaginationRun<T> {
     protected logger = logger;
     protected processedItems = 0;
 
-    constructor() {
-        logger.info(`Starting the process ${this.constructor.name}`);
+    public async dryRun(
+        act: {
+            fetchData: (page: number, pageSize: number) => any;
+            name?: string;
+            processItem: <T = any>(item: T, index: number) => any;
+            processFinished?: () => any;
+        },
+        pagination: { page: number; pageSize: number },
+    ) {
+        const { fetchData, name, processItem, processFinished = noop } = act;
+        const { page, pageSize } = pagination;
+        const logName = `[dryRun:${name ?? 'default'}]`;
+
+        const response = await fetchData(page, pageSize);
+
+        const { items, ...productResponse } = response as APIResponse;
+
+        logger.info(
+            `${logName} processing page: ${productResponse.page}/${productResponse.lastPage}`,
+        );
+
+        let index = 0;
+        for (const item of items) {
+            await processItem(item, index);
+
+            index++;
+            this.processedItems++;
+        }
+
+        if (productResponse.page === productResponse.lastPage) {
+            logger.info(`${logName} processed items ` + this.processedItems);
+            logger.info(`${logName} processed pages ` + page);
+
+            await processFinished();
+
+            return;
+        }
+
+        await this.dryRun(act, { page: page + 1, pageSize });
     }
 
     protected async fetchData(page: number, pageSize: number): Promise<T> {
@@ -28,8 +67,6 @@ export default class PaginationRun<T> {
             `Start Processing - Page: ${productResponse.page}/${productResponse.lastPage}`,
         );
 
-        console.time('process items');
-
         let index = 0;
         for (const item of items) {
             await this.processItem(item, index);
@@ -38,10 +75,8 @@ export default class PaginationRun<T> {
             index++;
         }
 
-        console.timeEnd('process items');
-
         if (productResponse.page === productResponse.lastPage) {
-            logger.info('Processed Items', this.processedItems);
+            logger.info('Processed Items' + this.processedItems);
 
             await this.processFinished();
 
